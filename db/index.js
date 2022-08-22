72ed4d828101c0c9a50a331b110f7d3b8fb05bbf
JohnsHarrison <jshar15089@gmail.com>
2022-08-22 13:06:13 -0400

    fix

---
+++ db/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/finalDatabase"
+
+
+mongoose.connect( MONGODB_URI,{
+    useUnifiedTopology: true, useNewUrlParser:true
+}).then(()=>{
+    console.log('successfully connected to MongoDB');
+}).catch(e =>{
+    console.error('connetion error', e.message)
+})
+
+//show the raw query mongo executes in the terminal
+mongoose.set('debug', true)
+
+const db = mongoose.connection
+
+module.exports= db
